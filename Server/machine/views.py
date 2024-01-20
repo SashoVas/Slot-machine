5211535ce17a6862629414c2d9ec9d9315db508c
SashoVas <98760930+SashoVas@users.noreply.github.com>
2024-01-20 15:15:02 +0200

    Initial commit

---
+++ Server/machine/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+# Create your views here.
+
+
+@api_view(["GET"])
+def test(request):
+    res = {"a": 1}
+    return Response(res)
